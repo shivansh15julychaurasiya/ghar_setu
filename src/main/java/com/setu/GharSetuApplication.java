fcbc340dac305f59e771ac24c9556e214bc479d4
Vijay <shivansh15julychaurasiya@gmail.com>
2026-07-28 10:46:06 +0530

    Hello this is my first commit

---
+++ src/main/java/com/setu/GharSetuApplication.java
@@ -0,0 +1,13 @@
+package com.setu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GharSetuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GharSetuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/setu/GharSetuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GharSetuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
